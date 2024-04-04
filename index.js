a888dbba1bd36bb5751dab0d1c2c3e34b2e7537e
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-04-04 12:04:18 -0700

    Sending Data with Fetch

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  });
+}
+then(response => {
+  if (!response.ok) {
+    throw new Error('Failed to submit data');
+  }
+  return response.json();
+})
+.then(data => {
+  document.getElementById('idElement').innerText = data.id;
+})
+.catch(error => {
+  document.getElementById('errorElement').innerText = error.message;
+});
+
